ec652adb2db43d58a94d50727a88ef53d79a51ef
tahir <tahirsaad73@gmail.com>
2022-12-19 11:20:50 +0500

    These are the task for PD_Week2

---
+++ Menu2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+system("Color 9A");
+cout<<" ================================================================="<<endl;
+cout<<"||                             MENU                              ||"<<endl;
+cout<<"||                                                               ||"<<endl;
+cout<<"||                          START GAME                           ||"<<endl;
+cout<<"||                          RESUME GAME                          ||"<<endl;
+cout<<"||                          LOAD GAME                            ||"<<endl;
+cout<<"||                          OPTIONS                              ||"<<endl;
+cout<<"||                          QUIT GAME                            ||"<<endl;
+cout<<"||                                                               ||"<<endl;
+cout<<" ================================================================="<<endl;
+return 0;
+}
---
+++ Task12.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+cout<<endl;
+cout<<"                       *     *     *              "<<endl;
+cout<<"                         *     *     *            "<<endl;
+cout<<"                          *     *     *           "<<endl;
+cout<<"                         *     *     *            "<<endl;
+cout<<"                        *     *     *             "<<endl;
+cout<<"                         *     *     *            "<<endl;
+cout<<"                           *     *     *          "<<endl;
+cout<<"            ************************************  "<<endl;
+cout<<"            *                                  *  "<<endl;
+cout<<"             *                                *   "<<endl;
+cout<<"               *                            *     "<<endl;
+cout<<"                 *                        *       "<<endl;
+cout<<"                   **********************         "<<endl;
+cout<<endl;
+cout<<endl;
+cout<<"  W E L C O M E  T O   F O O D Y   A P P L I C A T I O N"<<endl;
+return 0;
+}
